87782acb5447ffe5b06d3d5abb52cc49a36d31df
IceLez <benjamin.bergstrand@elev.ga.ntig.se>
2022-10-14 13:30:13 +0200

    KLAR Playermovement och deathfloor script i test
    Playermovement scriptet och deathfloor scriptet är redo för implementering i game environment.

---
+++ runAndJump/Assets/Testing/Benjamin/test/DeathFloor.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathFloor : MonoBehaviour
+{
+    public GameObject player;
+    public Transform target;
+    private Vector3 playerPos;
+
+    private float speed = 20f;
+
+    void Update()
+    {
+        playerPos = new Vector3(player.transform.position.x, transform.position.y);
+
+        transform.position = Vector3.MoveTowards(transform.position, playerPos, speed * Time.deltaTime);
+    }
+}
