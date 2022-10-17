f1d91ea1f1dbdc65d61212be919be1a713df8836
IsaacHog <111504782+IsaacHog@users.noreply.github.com>
2022-10-17 14:41:18 +0200

    Implementerar PlayerMovement till GameEnvironment

--- runAndJump/Assets/Testing/Isaac/PlayerMovementIsaac.cs
+++ runAndJump/Assets/Testing/Isaac/PlayerMovementIsaac.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementIsaac : MonoBehaviour
 {
     private float horizontal;
     private float speed = 8f;
