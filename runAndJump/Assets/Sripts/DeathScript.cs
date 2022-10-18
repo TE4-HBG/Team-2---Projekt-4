243263c2994d66fa69b5c7f1b4ca08c63d3a3f80
IsaacHog <111504782+IsaacHog@users.noreply.github.com>
2022-10-18 14:36:34 +0200

    Implementerat deathscript LÄS BESKRIVNING
    När vi skulle implementera deathscriptet fick vi göra mycket ändringar. Vi flyttade på scoredisplayen så att den var tydligare, vi la till att spelet pausas när spelaren dör. Vi la till att scenen laddas om när man trycker på restartknappen.

--- runAndJump/Assets/Sripts/DeathScript.cs
+++ runAndJump/Assets/Sripts/DeathScript.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DeathScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject Chaser;
     public GameObject Deathfloor;
+    public Text GameoverText;
+    public Text RestartText;
+    public Button Restartknapp;
+    public GameObject player;
+
+
+
     void Start()
     {
-
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -17,10 +24,50 @@ public class DeathScript : MonoBehaviour
         if (collision.gameObject == Deathfloor)
         {
             Debug.Log("game over you fell out");
+            GameOver("You fell out");
+
         }
         else if (collision.gameObject == Chaser)
         {
             Debug.Log("game over chaser killed you");
+            GameOver("The chaser killed you");
         }
     }
+
+    public void GameOver(string deathState)
+    {
+        Pause();
+        GameoverText.text = "GAME OVER" + "     " + deathState; // Visa highscore
+        RestartText.color = new Color(0, 0, 0, 255);
+        Restartknapp.image.enabled = true;
+        ColorBlock color = Restartknapp.colors;
+        color.normalColor = new Color(255, 255, 255, 255);
+        color.highlightedColor = new Color(255, 255, 255, 255);
+        Restartknapp.colors = color;
+    }
+
+    public void RestartKnapp()
+    {
+        GameoverText.text = "";
+        RestartText.color = new Color(0, 0, 0, 0);
+        ColorBlock color = Restartknapp.colors;
+        color.normalColor = new Color(255, 255, 255, 1);
+        color.highlightedColor = new Color(255, 255, 255, 0);
+        Restartknapp.colors = color;
+        Restartknapp.image.enabled = false;
+        player.transform.position = new Vector3(0, 2, 0);
+        Restart();
+    }
+
+    void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0f;
+    }
 }
+
