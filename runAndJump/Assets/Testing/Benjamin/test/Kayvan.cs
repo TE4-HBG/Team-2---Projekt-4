d15aacae926a7e76956bfef0e1e0aa45bbbbe90b
IceLez <benjamin.bergstrand@elev.ga.ntig.se>
2022-10-18 13:57:14 +0200

    KLAR chaser

---
+++ runAndJump/Assets/Testing/Benjamin/test/Kayvan.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kayvan : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 playerPos;
+    private float kayvanSpeed = 2;
+    
+    void Update()
+    {
+        playerPos = new Vector3(player.transform.position.x, player.transform.position.y);
+
+        transform.position = Vector3.MoveTowards(transform.position, playerPos, kayvanSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
