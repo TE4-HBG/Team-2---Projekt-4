5a7c9ee09e901dbb893210f77c32f1394e5fb168
Painini <tim.lofstedt@elev.ga.ntig.se>
2022-10-18 12:20:36 +0200

    Randomly spawning platforms Påbörjad
    I en testscen skapas det platformar automatiskt när spelet körs. Men scripten behöver fortfarande finslipas

---
+++ runAndJump/Assets/Testing/Tim/GeneratePlatforms.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePlatforms : MonoBehaviour
+{
+    public GameObject platformPrefab;
+    public GameObject playerObject;
+    private GameObject latestPlatform;
+    public int playerPlatformSpawnDist;
+    public int platformDist;
+    public int randValueY;
+    public int randStartValue;
+    public int randEndValue;
+    public int minValueY; //Add this to the code when you get back
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPlatformSpawnDist = 20;
+        platformDist = 8;
+        randStartValue = -3;
+        randEndValue = randStartValue + 6;
+
+        for (int i = 0; i < 3; i++)
+        {
+            randValueY = Random.Range(randStartValue, randEndValue);
+            latestPlatform = Instantiate(platformPrefab, new Vector3(platformDist, randValueY), Quaternion.identity);
+            platformDist += 8;
+            randStartValue = (int)latestPlatform.transform.position.y;
+            randEndValue = randStartValue + 6;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (latestPlatform.transform.position.x - playerObject.transform.position.x < playerPlatformSpawnDist)
+        {
+            randValueY = Random.Range(randStartValue, randEndValue);
+            latestPlatform = Instantiate(platformPrefab, new Vector3(platformDist, randValueY), Quaternion.identity);
+            platformDist += 8;
+            randStartValue = (int)latestPlatform.transform.position.y -3;
+            randEndValue = randStartValue + 6;
+        }
+    }
+}
