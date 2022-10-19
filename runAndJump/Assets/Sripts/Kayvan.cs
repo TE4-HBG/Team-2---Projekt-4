f37e357c765cd8abbbde0fcd0b567309b94b979d
IsaacHog <111504782+IsaacHog@users.noreply.github.com>
2022-10-19 10:31:34 +0200

    Implementerat instruktioner samt Spelet startar när spelaren lämnar spawn området

--- runAndJump/Assets/Sripts/Kayvan.cs
+++ runAndJump/Assets/Sripts/Kayvan.cs
@@ -7,12 +7,15 @@ public class Kayvan : MonoBehaviour
     public GameObject player;
     private Vector3 playerPos;
     private float kayvanSpeed = 10f;
-    
+
     void Update()
     {
         playerPos = new Vector3(player.transform.position.x, player.transform.position.y);
+        if (playerPos.x >= 2.5)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, playerPos, kayvanSpeed * Time.deltaTime);
+        }
 
-        transform.position = Vector3.MoveTowards(transform.position, playerPos, kayvanSpeed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider collision)
