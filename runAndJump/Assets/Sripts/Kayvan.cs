6511f28ece789056bef8a95bc9777c09191838bc
IceLez <benjamin.bergstrand@elev.ga.ntig.se>
2022-10-21 09:51:35 +0200

    Implementerat  Designa coola 3d-modeller för plattformarna

--- runAndJump/Assets/Sripts/Kayvan.cs
+++ runAndJump/Assets/Sripts/Kayvan.cs
@@ -6,16 +6,31 @@ public class Kayvan : MonoBehaviour
 {
     public GameObject player;
     private Vector3 playerPos;
+
+    private GameObject kayvan;
+    private Vector3 kayvanPos;
     private float kayvanSpeed = 12f;
 
     void Update()
     {
+        kayvan = this.gameObject;
+
         playerPos = new Vector3(player.transform.position.x, player.transform.position.y);
+        kayvanPos = new Vector3(kayvan.transform.position.x, player.transform.position.y);
+
         if (playerPos.x >= 2.5)
         {
             transform.position = Vector3.MoveTowards(transform.position, playerPos, kayvanSpeed * Time.deltaTime);
         }
 
+        if (playerPos.x - 250f >= kayvanPos.x)
+        {
+            kayvanSpeed = 30f;
+        }
+        else
+        {
+            kayvanSpeed = 12f;
+        }
     }
 
     private void OnTriggerEnter(Collider collision)
