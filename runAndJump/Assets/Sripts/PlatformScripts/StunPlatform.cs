5648bc6ed458d9c1db59a7640515d15a3dac9eda
Painini <tim.lofstedt@elev.ga.ntig.se>
2022-10-25 09:52:54 +0200

    KLAR Skapa en plattform som kedjar en till plattformen en viss tid

---
+++ runAndJump/Assets/Sripts/PlatformScripts/StunPlatform.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+
+public class StunPlatform : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody rb;
+    private PlayerMovement playerMovementScript;
+    private int currentTimer;
+    private int timer;
+    bool isTouched;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        player = GameObject.Find("CoffeeMug");
+        playerMovementScript = player.GetComponent<PlayerMovement>();
+        rb = player.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer++;
+        if (currentTimer + 600 >= timer && isTouched)
+        {
+            playerMovementScript.speed = 0f;
+            playerMovementScript.jumpingPower = 0f;
+        }
+        else if (currentTimer + 600 < timer && isTouched)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 20);
+            isTouched = false;
+            playerMovementScript.speed = 16f;
+            playerMovementScript.jumpingPower = 10f;
+        }
+        
+       
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!isTouched)
+        {
+            currentTimer = timer;
+            isTouched = true;
+        }
+        
+       
+    }
+}
